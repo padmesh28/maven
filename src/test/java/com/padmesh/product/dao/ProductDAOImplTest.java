8d89c4f871f3506c9c4a3d0048a8b7b0160735a2
Tripathi <GQ10FQ@ad.ing.net>
2017-01-18 20:08:53 +0100

    change ihone to iphone

--- src/test/java/com/padmesh/product/dao/ProductDAOImplTest.java
+++ src/test/java/com/padmesh/product/dao/ProductDAOImplTest.java
@@ -14,7 +14,7 @@ public class ProductDAOImplTest {
   ProductDAO dao = new ProductDAOImpl();
   Product product = new Product();
   product.setId(1);
-  product.setName("Ipone");
+  product.setName("Iphone");
   product.setDescription("its awesome");
   product.setPrice(800);
   dao.create(new Product());
