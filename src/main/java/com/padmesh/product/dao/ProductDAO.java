d5764b20dd3f6cd7e585431aa34381bb3612c176
Tripathi <GQ10FQ@ad.ing.net>
2017-01-21 23:03:35 +0100

    test

--- src/main/java/com/padmesh/product/dao/ProductDAO.java
+++ src/main/java/com/padmesh/product/dao/ProductDAO.java
@@ -13,16 +13,3 @@ public interface ProductDAO {
 	void delete(int id);
 }
 
-
-
-public interface ProductDAO {
-	
-	void create(Product product);
-
-	Product  read(int id);
-	
-	void update(Product product);
-	
-	void delete(int id);
-}
-
