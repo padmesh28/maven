977490f61a9bcc6379445de326b20d7ccf011af2
Tripathi <GQ10FQ@ad.ing.net>
2017-01-22 22:06:59 +0100

    test

--- src/main/java/com/padmesh/product/bo/ProductBOImpl.java
+++ src/main/java/com/padmesh/product/bo/ProductBOImpl.java
@@ -4,11 +4,8 @@ import com.padmesh.product.dao.ProductDAO;
 import com.padmesh.product.dto.Product;
 
 public class ProductBOImpl implements ProductBO {
-
-	
-	private ProductDAO dao;
-	
-	public void create(Product product) {
+private ProductDAO dao;
+public void create(Product product) {
 		dao.create(product);
 
 	}
