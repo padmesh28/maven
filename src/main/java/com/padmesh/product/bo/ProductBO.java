095d18a3435406406b5044d2201c89a605ee4837
Tripathi <GQ10FQ@ad.ing.net>
2017-01-22 22:11:09 +0100

    test

--- src/main/java/com/padmesh/product/bo/ProductBO.java
+++ src/main/java/com/padmesh/product/bo/ProductBO.java
@@ -1,11 +1,7 @@
 package com.padmesh.product.bo;
-
 import com.padmesh.product.dto.Product;
-
-public interface ProductBO {
-	public void create(Product product);
-	
-	public Product findProduct (int id);
-	
-
+public interface ProductBO
+{
+public void create(Product product);
+public Product findProduct (int id);
 }
--- src/main/java/com/padmesh/product/bo/ProductBOImpl.java
+++ src/main/java/com/padmesh/product/bo/ProductBOImpl.java
@@ -1,25 +1,24 @@
 package com.padmesh.product.bo;
-
 import com.padmesh.product.dao.ProductDAO;
 import com.padmesh.product.dto.Product;
 
-public class ProductBOImpl implements ProductBO {
+public class ProductBOImpl implements ProductBO
+{
 private ProductDAO dao;
-public void create(Product product) {
-		dao.create(product);
-
-	}
-
-	public Product findProduct(int id) {
-		return dao.read(id);
-	}
-
-	public ProductDAO getDao() {
-		return dao;
-	}
-
-	public void setDao(ProductDAO dao) {
-		this.dao = dao;
-	}
-
+public void create(Product product)
+{
+dao.create(product);
+}
+public Product findProduct(int id)
+{
+return dao.read(id);
+}
+public ProductDAO getDao()
+{
+return dao;
+}
+public void setDao(ProductDAO dao)
+{
+this.dao = dao;
+}
 }
